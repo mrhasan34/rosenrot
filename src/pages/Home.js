3af3c64fb61226c4e0706e6a5f83bc59ce5f3245
Hasan Çavuş <hasancvs98@gmail.com>
2024-07-07 23:30:03 +0300

    1.2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home">
-      <h1>HOMEPAGE</h1>
+      <h1>Berrin'mmm</h1>
       <p>Bedişim hoşgeldin siteme <3 <3 <3 <3 .</p>
     </div>
   );
