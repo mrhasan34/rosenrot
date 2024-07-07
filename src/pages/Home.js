7260b5cc9cba59609b015b68dd2cdb98a0645624
Hasan Çavuş <hasancvs98@gmail.com>
2024-07-07 23:33:14 +0300

    1.3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Berrin'mmm</h1>
-      <p>Bedişim hoşgeldin siteme <3 <3 <3 <3 .</p>
+      <p>Bedişim hoşgeldin siteme.</p>
     </div>
   );
 };
